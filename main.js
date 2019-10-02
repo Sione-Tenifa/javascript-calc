002b6e2e0e3a38f8bb3d2b2b82b218562f6bd986
Sione Tenifa <johntenifa@yahoo.com>
2019-10-01 23:34:39 -0600

    operations

--- main.js
+++ main.js
@@ -18,20 +18,44 @@ class Calculator {
 
   appendNumber(number){
     if ( number === '.' && this.currentOperand.includes('.')) return
-    if (this.previousOperand !== ''(
-      this.compute()
-    ))
     this.currentOperand = this.currentOperand.toString() + number.toString()
   }
 
   chooseOperation(operation){
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== ''){
+      this.compute()
+    }
     this.operation = operation
     this.previousOperand = this.currentOperand
     this.currentOperand = ''
   }
+      
   
   compute() {
-
+    let computation 
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+      switch (this.operation) {
+        case '+':
+          computation = prev + current
+          break
+        case '-':
+          computation = prev - current
+          break
+        case '/':
+          computation = prev / current
+          break
+        case '*':
+          computation = prev * current
+          break
+        default: 
+          return
+      }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
   }
 
   updateDisplay(){
@@ -68,4 +92,9 @@ operationButtons.forEach(button => {
     calculator.updateDisplay()  
     // console.log(updateDisplay)
   })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
 })
